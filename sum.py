0dcf718e4d0fa97cb3a221df1664cc59165801ad
Ivan <forgit101@yandex.ru>
2022-10-03 01:44:26 +0300

    fix a bug summ

--- sum.py
+++ sum.py
@@ -1,5 +1,9 @@
-
+def summ (a,b):
+    return a + b
+def summ (a, b, c):
+    return a + b + c
+def summ (a, b, c, d):
+    return a + b + c +d
 def summ (a, b, c, d, e):
     return a + b + c + d + e
-
     
